7a74d6d84e09b6e4589a554c1d5de91e46998e7a
Naaikiru <kiki010907@mail.ru>
2026-05-14 23:44:40 +1000

    8lab start

---
+++ lab_8/lab8.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct humen
+{
+	char name[20];
+	char lastn[20];
+	int bthd;
+    char gendr[20];
+    int hght;
+};
+
+
+int main()
+{
+    int count = 0;
+	char bff[256];
+	FILE * f1 = fopen("info_about_you.txt", "r");
+	while (fgets(bff, sizeof(bff), f1) != NULL) 
+	{
+        if (strlen(bff) > 1) 
+		{
+            count++;
+        }
+    }
+	fclose(f1);
+
+    
+
+    return 0;
+}
