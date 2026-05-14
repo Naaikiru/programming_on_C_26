35275903a2096c0f7d2c900f47e9e083378c7587
Naaikiru <kiki010907@mail.ru>
2026-05-14 23:24:23 +1000

    sa

--- lab_7/lab7.c
+++ lab_7/lab7.c
@@ -63,43 +63,43 @@ struct humen
 
 int main() 
 {
-	int chlen = 0;
+	int count = 0;
 	char bff[256];
 	FILE * f1 = fopen("nlnage.txt", "r");
 	while (fgets(bff, sizeof(bff), f1) != NULL) 
 	{
         if (strlen(bff) > 1) 
 		{
-            chlen++;
+            count++;
         }
     }
 	fclose(f1);
 	
 	struct humen *kek;
-    kek = (struct humen*)malloc(chlen*sizeof(struct humen));
+    kek = (struct humen*)malloc(count*sizeof(struct humen));
 	
     struct humen *lol;
-    lol = (struct humen*)malloc(chlen*sizeof(struct humen));
+    lol = (struct humen*)malloc(count*sizeof(struct humen));
     
 	int i, j;
 
     FILE * f2 = fopen("nlnage.txt", "r");
     
-    for(i = 0; i < chlen; i++)
+    for(i = 0; i < count; i++)
     {
         fscanf(f2, "%s %s %d", kek[i].name, kek[i].lastn, &kek[i].bthd);
     } 
 
     fclose(f2);
 
-    for (i = 0; i < chlen; i++) 
+    for (i = 0; i < count; i++) 
     {
         lol[i] = kek[i];
     }
 
-    for(i = 0; i < chlen - 1; i++)
+    for(i = 0; i < count - 1; i++)
     {
-        for (j = 0; j < chlen - 1 - i; j++)
+        for (j = 0; j < count - 1 - i; j++)
         {
             if (lol[j].bthd < lol[j + 1].bthd)
             {
@@ -110,7 +110,7 @@ int main()
         }
     }
     
-    for(j = 0; j < chlen; j++)
+    for(j = 0; j < count; j++)
     {
     	printf("%s %s %d\n", lol[j].name, lol[j].lastn, lol[j].bthd);
 	}
@@ -119,3 +119,4 @@ int main()
 	free(lol);
 	
 	return 0;
+}
