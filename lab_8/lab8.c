a4eeeea932ac50f5fd2e634f2d16ac46cc703395
Naaikiru <kiki010907@mail.ru>
2026-05-15 15:04:15 +1000

    name

--- lab_8/lab8.c
+++ lab_8/lab8.c
@@ -39,6 +39,26 @@ int main()
 	}
 	fclose(f2);
 
+	char op;
+	printf("enter the organization parameter(Name/LastName, BDay, Gender, Height(only capital letters & u can use several options)):\n");
+	scanf("%c", &op);
+
+	if (op == 'N')
+	{
+		for (i = 0; i < count - 1; i++)
+		{
+			for (j = 0; j < count - 1 - i; j++)
+			{
+				if (strcmp(kek[j].name, kek[j + 1].name) > 0)
+				{
+					struct humen meow = kek[j];
+					kek[j] = kek[j + 1];
+					kek[j + 1] = meow; 
+				}
+			}
+		}
+	}
+
 	for (i = 0; i < count; i++)
 	{
 		printf("%s %s %d %s %g\n", kek[i].name, kek[i].lastn, kek[i].bthd, kek[i].gendr, kek[i].hght);
