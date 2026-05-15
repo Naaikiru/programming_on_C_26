180181323f9ae21e0187bd372dbe4031480fd14e
Naaikiru <kiki010907@mail.ru>
2026-05-16 01:46:31 +1000

    mb thats right

--- lab_10/lab10.c
+++ lab_10/lab10.c
@@ -2,38 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Макрос для нахождения максимального числа в массиве
-#define FIND_MAX(arr, n) ({ \
-    int _max = (arr)[0]; \
-    for(int _i = 1; _i < (n); _i++) { \
-        if((arr)[_i] > _max) _max = (arr)[_i]; \
-    } \
-    _max; \
-})
+#define MAX_CHISLO(chisla, count) ({int per = (chisla)[0]; for(int w = 1; w < (count); w++) {if((chisla)[w] > per) per = (chisla)[w]; } per;})
 
 int main() {
-    char input[1000];
+    char stroka[512];
     int numbers[100];
-    int count = 0;
-    char *token;
+    int i;
+    int cunt;
+
+    printf("if you wanna enter a chisla, so you need to give me a count: ");
+    scanf("%d", &cunt);
     
-    printf("Введите числа через пробел: ");
-    fgets(input, sizeof(input), stdin);
-    
-    token = strtok(input, " \t\n");
-    while(token != NULL && count < 100) {
-        numbers[count] = atoi(token);
-        count++;
-        token = strtok(NULL, " \t\n");
+    printf("enter chisla with probel: ");
+    for (i = 0; i < cunt; i++)
+    {
+        scanf(" %c", &stroka);
     }
+
     
-    if(count == 0) {
-        printf("Числа не введены!\n");
-        return 1;
-    }
-    
-    int max_number = FIND_MAX(numbers, count);
-    printf("Максимальное число: %d\n", max_number);
     
     return 0;
 }
