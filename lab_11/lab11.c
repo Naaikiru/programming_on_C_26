b8018b6b13b6db856a4c844dbbbb4f80b1e3058f
Naaikiru <kiki010907@mail.ru>
2026-05-23 01:10:32 +1000

    final

--- lab_11/lab11.c
+++ lab_11/lab11.c
@@ -1,9 +1,13 @@
+#include "functions.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
+    printf("summa: %d\n", summ(5, 10, 7, 12, 2, 19));
+    printf("maximum: %d\n", maxx(5, 10, 7, 12, 2, 19));
+    printf("minimum: %d\n", minn(5, 10, 7, 12, 2, 19));
+    printf("srednee znachenie: %d\n", srznach(5, 10, 7, 12, 2, 19));
     
-
     return 0;
 }
