a06699588ce54a8c6a9adbe3f4a3179a7f46f959
Naaikiru <kiki010907@mail.ru>
2026-05-23 00:59:14 +1000

    maxminsrznach

--- lab_11/functions.c
+++ lab_11/functions.c
@@ -19,10 +19,51 @@ int summ(int n, ...);
 
 int maxx(int n, ...);
 {
-
+    int res = 0;
+    va_list list;
+    va_start(list, n);
+    while(n)
+    {
+        int max = va_arg(list, int);
+        if (max > res)
+        {
+            res = max;
+        }
+        --n;
+    }
+    va_end(list);
+    return res;
 }
 
 int minn(int n, ...);
-
+{
+    int res = 0;
+    va_list list;
+    va_start(list, n);
+    while(n)
+    {
+        int max = va_arg(list, int);
+        if (max < res)
+        {
+            res = max;
+        }
+        --n;
+    }
+    va_end(list);
+    return res;
+}
 
 int srznach(int n, ...);
+{
+    int res = 0;
+    va_list list;
+    va_start(list, n);
+    while(n)
+    {
+        res += va_arg(list, int);
+        --n;
+    }
+    res = res/n;
+    va_end(list);
+    return res;
+}
