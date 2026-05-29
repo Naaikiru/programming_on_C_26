ab00a3bfee062f246c38412448495aca5605ca4f
Naaikiru <kiki010907@mail.ru>
2026-05-29 20:03:05 +1000

    peremennii

--- lab_9/lab9.c
+++ lab_9/lab9.c
@@ -6,10 +6,16 @@
 int main()
 {
     printf("|| calculator ||\n");
+
     int i;
-    int chi[100];
+    int frst = 1;
+    int minus = 0;
+    double res = 0.0;
+    double vrem;
+    char vremop = '=';
     char stroka[256];
     char *token;
+    char *prop;
 
     printf("enter primer with probel: ");
     fgets(stroka, sizeof(stroka), stdin);
--- test.c
+++ test.c
@@ -1,122 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-
-double calculate(const char *expression) {
-    char *str = malloc(strlen(expression) + 2);
-    if (!str) {
-        printf("Ошибка выделения памяти\n");
-        return 0;
-    }
-    strcpy(str, expression);
-    
-    double result = 0.0;
-    double current_number;
-    char *token;
-    char last_operator = '+';
-    int is_first = 1;
-    int unary_mode = 0;
-    
-    // Разделители: пробел, табуляция и т.д.
-    token = strtok(str, " \t\n");
-    
-    while (token != NULL) {
-        char *endptr;
-        
-        // Проверяем, является ли токен оператором
-        if (strlen(token) == 1 && (token[0] == '+' || token[0] == '-')) {
-            last_operator = token[0];
-            unary_mode = 0;
-        }
-        else {
-            // Пытаемся преобразовать токен в число
-            current_number = strtod(token, &endptr);
-            
-            // Если преобразование не удалось
-            if (*endptr != '\0') {
-                // Проверка на унарный оператор в начале токена
-                if ((token[0] == '+' || token[0] == '-') && strlen(token) > 1) {
-                    char *num_str = token + 1;
-                    current_number = strtod(num_str, &endptr);
-                    
-                    if (*endptr != '\0') {
-                        printf("Ошибка: некорректное число '%s'\n", token);
-                        free(str);
-                        return 0;
-                    }
-                    
-                    // Применяем унарный оператор
-                    if (token[0] == '-') {
-                        current_number = -current_number;
-                    }
-                    
-                    // Определяем операцию для этого числа
-                    if (is_first) {
-                        result = current_number;
-                        is_first = 0;
-                    } else {
-                        if (last_operator == '+')
-                            result += current_number;
-                        else if (last_operator == '-')
-                            result -= current_number;
-                    }
-                    last_operator = '+';
-                }
-                else {
-                    printf("Ошибка: некорректный токен '%s'\n", token);
-                    free(str);
-                    return 0;
-                }
-            }
-            else {
-                // Обычное число
-                if (is_first) {
-                    result = current_number;
-                    is_first = 0;
-                } else {
-                    if (last_operator == '+')
-                        result += current_number;
-                    else if (last_operator == '-')
-                        result -= current_number;
-                }
-                last_operator = '+';
-            }
-        }
-        
-        token = strtok(NULL, " \t\n");
-    }
-    
-    free(str);
-    return result;
-}
+#include <math.h>
 
 int main() {
-    char expression[1024];
+    char input[256];
+    char *token;
+    double result = 0.0;
+    double current_number;
+    char current_operator = '+';
+    int first_number = 1;
+    int unary_minus = 0;
+    char *ptr;
+    int i;
     
-    printf("Калькулятор (поддерживает + и -, унарные операторы)\n");
-    printf("Примеры: \"-123.5 + 4 - 456+56\" или \"5+-3\"\n");
-    printf("Введите 'exit' для выхода\n\n");
+    printf("Введите арифметическое выражение (например, -123.5 + 4 - 456+56): ");
+    fgets(input, sizeof(input), stdin);
     
-    while (1) {
-        printf("Введите выражение: ");
-        fgets(expression, sizeof(expression), stdin);
-        
-        // Удаляем символ новой строки
-        expression[strcspn(expression, "\n")] = '\0';
-        
-        if (strcmp(expression, "exit") == 0) {
-            printf("До свидания!\n");
-            break;
-        }
-        
-        if (strlen(expression) == 0) {
+    // Удаляем символ новой строки, если он есть
+    size_t len = strlen(input);
+    if (len > 0 && input[len-1] == '\n') {
+        input[len-1] = '\0';
+    }
+    
+    // Разбиваем строку на токены
+    token = strtok(input, " ");
+    
+    while (token != NULL) {
+        // Проверяем, является ли токен оператором
+        if (strlen(token) == 1 && (token[0] == '+' || token[0] == '-')) {
+            current_operator = token[0];
+            token = strtok(NULL, " ");
             continue;
         }
         
-        double result = calculate(expression);
-        printf("Результат: %g\n\n", result);
+        // Обработка чисел (включая отрицательные)
+        current_number = 0.0;
+        i = 0;
+        unary_minus = 0;
+        
+        // Проверка на унарный минус в начале токена
+        if (token[0] == '-') {
+            unary_minus = 1;
+            ptr = token + 1; // Пропускаем минус
+        } else {
+            ptr = token;
+        }
+        
+        // Преобразуем строку в число
+        current_number = strtod(ptr, NULL);
+        
+        // Если был унарный минус, делаем число отрицательным
+        if (unary_minus) {
+            current_number = -current_number;
+        }
+        
+        // Выполняем операцию
+        if (first_number) {
+            result = current_number;
+            first_number = 0;
+        } else {
+            if (current_operator == '+') {
+                result += current_number;
+            } else if (current_operator == '-') {
+                result -= current_number;
+            }
+        }
+        
+        token = strtok(NULL, " ");
     }
+    
+    printf("Результат: %g\n", result);
     
     return 0;
 }
