9bf8b351997c6be94210f772f7dd77da0a122be4
Naaikiru <kiki010907@mail.ru>
2026-06-06 01:23:58 +1000

    13131313

--- lab_13/lab13.c
+++ lab_13/lab13.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 
 void handler(int sig)
 {
     if (sig == SIGABRT)
     {
         printf("Signal handler SIGABRT: %d.\n", sig);
+        exit(0);
     }
     else if (sig == SIGFPE)
     {
         printf("Signal handler SIGFPE: %d.\n", sig);
+        exit(0);
     }
     else if (sig == SIGILL)
     {
         printf("Signal handler SIGILL: %d.\n", sig);
+        exit(0);
     }
     else if (sig == SIGINT)
     {
         printf("Signal handler SIGINT: %d.\n", sig);
+        exit(0);
     }
     else if (sig == SIGSEGV)
     {
         printf("Signal handler SIGSEGV: %d.\n", sig);
+        exit(0);
     }
     else if (sig == SIGTERM)
     {
         printf("Signal handler SIGTERM: %d.\n", sig);
+        exit(0);
     }
 }
 
@@ -52,42 +59,46 @@ int main(void)
         {
             return 0;
         }
-        else if(chc == 1)
+        else if(chc == 1)//	Сигнал аварийного завершения процесса.
         {
             signal(SIGABRT, handler);
-            printf("raise signal\n");
-            raise(SIGABRT);
+            printf("calling abort()...\n");
+            abort();
             signal(SIGABRT, SIG_DFL);
         }
-        else if(chc == 2)
+        else if(chc == 2)//Некорректная арифметическая операция.
         {
             signal(SIGFPE, handler);
-            printf("raise signal\n");
-            raise(SIGFPE);
+            int a = 5, b = 0;
+            printf("dividing by zero...\n");
+            int c = a / b;
             signal(SIGFPE, SIG_DFL);
         }
-        else if(chc == 3)
+        else if(chc == 3)//Некорректная команда.
         {
             signal(SIGILL, handler);
-            printf("raise signal\n");
-            raise(SIGILL);
+            printf("illegal instruction...\n");
+            __builtin_trap();
             signal(SIGILL, SIG_DFL);
         }
-        else if(chc == 4)
+        else if(chc == 4)//	Сигнал прерывания
         {
             signal(SIGINT, handler);
-            printf("raise signal\n");
-            raise(SIGINT);
-            signal(SIGINT, SIG_DFL);
+            while(1)
+            {
+                printf("hello mister fish!\n");
+                sleep(1);
+            }
         }
-        else if(chc == 5)
+        else if(chc == 5)//Некорректное обращение к памяти.
         {
             signal(SIGSEGV, handler);
-            printf("raise signal\n");
-            raise(SIGSEGV);
+            int *ptr = NULL;
+            printf("NULL pointer...\n");
+            *ptr = 67;
             signal(SIGSEGV, SIG_DFL);
         }
-        else if(chc == 6)
+        else if(chc == 6)//Подразумеваемая реакция - аварийное завершение процесса
         {
             signal(SIGTERM, handler);
             printf("raise signal\n");
@@ -99,8 +110,7 @@ int main(void)
             printf("wrong enter!\n");
             continue;
         }
-        printf("OK\n");
     }
     
     return 0;
-}// вызвать физически в самом мейне.
+}
