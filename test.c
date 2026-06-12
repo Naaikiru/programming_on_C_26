a06ac80c0b6468b7dd0bfc8c6346b889ebc0a5b2
Naaikiru <kiki010907@mail.ru>
2026-06-12 20:17:09 +1000

    вапр

--- test.c
+++ test.c
@@ -1,13 +1,34 @@
-#include <signal.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
+
+/*Написать программу вычисления максимального числа из введенных пользователем в виде одной строки.
+Использовать для этого макрос (без функции), на вход которому подается массив чисел и их количество.*/
+
+#define MAX(array, n) ({int max = (array)[0]; for (int i = 1; i < (n); i++) {if ((array)[i] > max) max = (array)[i];} max;})
 
 int main() {
-    pid_t pid = getpid(); // получение идентификатора текущего процесса
-    if (kill(pid, SIGTERM) == -1) {
-        perror("Ошибка отправки сигнала");
-        exit(1);
+    char input[10000];
+    int numbers[1000];
+    int cnt = 0;
+    
+    printf(" Enter the numbers: ");
+    if (fgets(input, sizeof(input), stdin) == NULL) {
+        printf(" Input error!");
+        return 1;
     }
+
+    char *wlw = strtok(input, " \n");
+    while (wlw != NULL && cnt < 1000) {
+        int num = atoi(wlw);
+        numbers[cnt] = num;
+        cnt++;
+        wlw = strtok(NULL, " \n");
+    }
+
+    int max = MAX(numbers,cnt);
+
+    printf(" Max number: %d", max);
+
     return 0;
 }
